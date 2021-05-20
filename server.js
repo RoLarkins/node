8bc107dc99d6b0184cdd28e88106f600fe5db9be
Roosevelt <LarkinsRo@gmail.com>
2021-05-20 08:29:07 -0400

    work

--- server.js
+++ server.js
@@ -1,15 +1,44 @@
 const http = require('http');
 const fs = require('fs')
-
-
+const _ = require('lodash');
 const server = http.createServer((req,res) => {
-    console.log('first',req.url,'second', req.method);
+   //lodash
+   const num = _.random(0,20);
+   console.log(num);
+
+
+    console.log(req.url, req.method);
 
 
     res.setHeader('Content-Type', 'text/html')
 
+    let path = './views/';
+    switch(req.url){
+        case '/':
+        path += 'index.html';
+        res.statusCode = 200;
+        break;
+     case '/about':
+      path += 'about.html';
+      res.statusCode = 200;
+
+      case '/about-me':
+      res.statusCode = 301;
+      res.setHeader('Location', '/about');
+      res.end();
+
+      
+      default:
+      path += '404.html';
+      res.statusCode = 404;
+      break;
+
+    
+    }
+    
+
     //send an html file
-    fs.readFile('./views/index.html', (err, data) =>{
+    fs.readFile(path, (err, data) =>{
         if (err) {
             console.log.toString(err);
         
